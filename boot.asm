9b385ffce59d0461c696963d1724cd7cb8924f12
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-07-21 15:01:32 +0900

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -2,20 +2,18 @@
 [bits 16]
 	jmp 0x07C0:start
 start:
-	mov ax, 0xb800
-	mov es, ax
-	mov byte[es:0], 'L'
-	mov byte[es:1], 0x0F
-	mov byte[es:2], 'A'
-	mov byte[es:3], 0x0F
-	mov byte[es:4], 'P'
-	mov byte[es:5], 0x0F
-	mov byte[es:6], 'A'
-	mov byte[es:7], 0x0F
-	mov byte[es:8], 'S'
-	mov byte[es:9], 0x0F
+	mov ax, 0x1000 ; es:bx 1000:0000 => segment:offset
+	mov es, ax ;set es to 1000
+	mov bx, 0 ;set bx to 0
 
-	jmp $
+	mov ah, 2 ;fetch the es:bx address
+	mov al, 1 ;read sector 1 => bootmaster Sector
+	mov ch, 0
+	mov cl, 2 ;read sector 2 => kernel Sector
+	mov dh, 0 ;head to 0
+	mov dl, 0
+	int 0x13 ;read Sectors
 
-	times 510 - ($ - $$) db 0
-	dw 0xAA55
+	jc read;if Read error occur, re-start to read Kernel
+	
+	jmp 0x1000:0000 ;this is es:bx
